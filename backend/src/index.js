d56d651866a5a283ea229ed67e43dcf4317a5138
brunoolira1990 <bruno.lira@icloud.com>
2025-10-23 10:29:37 -0300

    23/10/2025 - 10:29

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,9 @@ const app = express();
 
 // Configuração específica de CORS para permitir apenas o domínio do frontend
 const corsOptions = {
-  origin: 'https://nexusvalvulas.com.br',
+  origin: process.env.NODE_ENV === 'development' 
+    ? ['http://localhost:3000', 'http://localhost:5173', 'https://nexusvalvulas.com.br']
+    : 'https://nexusvalvulas.com.br',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.20m9f9cu0so"
+    "revision": "0.9novplj00kg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
