fa2c44ac23aca163262d824809f7d3cc7a3726f9
brunoolira1990 <bruno.liraa@icloud.com>
2026-01-29 11:01:24 -0300

    Deploy producao com WhiteNoise

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,10 +2,7 @@
 URL configuration for nexus_valvulas project.
 """
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,52 +10,3 @@ urlpatterns = [
     path('api/products/', include('apps.products.urls')),
     path('api/blog/', include('apps.blog.urls')),
 ]
-
-# Serve static files in development
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-"""
-from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Autenticação
-    path('api/products/', include('apps.products.urls')),
-    path('api/blog/', include('apps.blog.urls')),
-]
-
-# Serve static files in development
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
-"""
-from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Autenticação
-    path('api/products/', include('apps.products.urls')),
-    path('api/blog/', include('apps.blog.urls')),
-]
-
-# Serve static files in development
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
